3dda02a37b9dad0fc83fab91a9bc8c316b0fc336
David Morales <demorales@email.arizona.edu>
2021-11-15 08:11:14 -0700

    Reformatted code and updated markdown
    Shp and gdb data read-in code relocated to beginning of script. Unnecessary lines cleaned up and appropriate comments added for clarity.

--- Forecast_Submissions/team_forecast/Team3_Forecast.py
+++ Forecast_Submissions/team_forecast/Team3_Forecast.py
@@ -35,6 +35,16 @@ strfdata = nwis.get_record(sites=station_id,
                            parameterCd='00060')
 strfdata.columns = ['flow', 'agency_cd', 'site_no']
 
+# Read in shapefile of gage locations for map
+file = os.path.join('..', '..', 'data', "gagesII_9322_point_shapefile",
+                    'gagesII_9322_sept30_2011.shp')
+gages = gpd.read_file(file)
+
+# Reading in a geodataframe for map
+file = os.path.join('..', '..', 'data', 'NHDPLUS_H_1506_HU4_GDB.gdb')
+fiona.listlayers(file)
+HUC6 = gpd.read_file(file, layer="WBDHU6")
+
 
 # %%
 # Function for linear regression model estimating one/two week flows
@@ -120,55 +130,39 @@ plt.show()
 fig.savefig('prcp_flow.jpg', dpi=350, bbox_inches='tight')
 
 # %%
-file = os.path.join('..','..', 'data',"gagesII_9322_point_shapefile",
-                    'gagesII_9322_sept30_2011.shp')
-gages = gpd.read_file(file)
+# Prepare map from read-in data:
+# Select gages within Arizona state boundaries
+gages_AZ = gages[gages['STATE'] == 'AZ']
 
-type(gages)
-gages.head()
-gages.columns
-gages.shape 
-
-# %% 
-gages.STATE.unique()
-gages_AZ=gages[gages['STATE']=='AZ']
-gages_AZ.shape
-
-# Reading in a geodataframe
-file = os.path.join('..','..', 'data', 'NHDPLUS_H_1506_HU4_GDB.gdb')
-fiona.listlayers(file)
-HUC6 = gpd.read_file(file, layer="WBDHU6")
-
-# %%
-# Added A point
-# Stream gauge in Camp Verde:  34.44833333, -111.7891667
+# Added stream gauge in Camp Verde
 point_list = np.array([[-111.7891667, 34.44833333]])
-#make these into spatial features
+# Convert point into spatial feature
 point_geom = [Point(xy) for xy in point_list]
-point_geom
-
-#map a dataframe of these points
-point_df = gpd.GeoDataFrame(point_geom, columns=['geometry'],
+# Map a dataframe of these points
+point_df = gpd.GeoDataFrame(point_geom,
+                            columns=['geometry'],
                             crs=HUC6.crs)
 
-# %%
-# CRS = Coordinate Reference System
-HUC6.crs
-gages.crs
-
-# Projecting the point
+# Reproject spatial data to NAD83
 points_project = point_df.to_crs(gages_AZ.crs)
-
 HUC6_project = HUC6.to_crs(gages_AZ.crs)
 
 # %%
-# Final Map
+# Generate map
 fig, ax = plt.subplots(figsize=(7, 10))
-gages_AZ.plot(column='DRAIN_SQKM', categorical=False,
-              legend=True, markersize=25, cmap='terrain',
+gages_AZ.plot(column='DRAIN_SQKM',
+              categorical=False,
+              legend=True,
+              markersize=25,
+              cmap='terrain',
               ax=ax)
-points_project.plot(ax=ax, color='red', marker='X')
-HUC6_project.boundary.plot(ax=ax, color=None,
-                           edgecolor='black', linewidth=.3)
-ctx.add_basemap(ax, crs=gages_AZ.crs, source = ctx.providers.OpenTopoMap)
+points_project.plot(ax=ax,
+                    color='red',
+                    marker='X')
+HUC6_project.boundary.plot(ax=ax,
+                           color=None,
+                           edgecolor='black',
+                           linewidth=.3)
+ctx.add_basemap(ax, crs=gages_AZ.crs,
+                source=ctx.providers.OpenTopoMap)
 fig.savefig("Group_Proj_Watershed")
