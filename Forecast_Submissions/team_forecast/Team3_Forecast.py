8304e04dd3967da57f21fdb7029a402dbc15bc63
kevincdyer <kevin.dyer.nj@gmail.com>
2021-11-15 00:03:31 -0700

    Added Script to right place, renamed as well

--- Forecast_Submissions/team_forecast/Team3_Forecast.py
+++ Forecast_Submissions/team_forecast/Team3_Forecast.py
@@ -15,6 +15,8 @@ from sklearn.linear_model import LinearRegression
 import datetime
 import dataretrieval.nwis as nwis
 import os
+from shapely.geometry import Point
+import contextily as ctx
 
 # %%
 # Read in necessary data:
@@ -118,3 +120,55 @@ plt.show()
 fig.savefig('prcp_flow.jpg', dpi=350, bbox_inches='tight')
 
 # %%
+file = os.path.join('..','..', 'data',"gagesII_9322_point_shapefile",
+                    'gagesII_9322_sept30_2011.shp')
+gages = gpd.read_file(file)
+
+type(gages)
+gages.head()
+gages.columns
+gages.shape 
+
+# %% 
+gages.STATE.unique()
+gages_AZ=gages[gages['STATE']=='AZ']
+gages_AZ.shape
+
+# Reading in a geodataframe
+file = os.path.join('..','..', 'data', 'NHDPLUS_H_1506_HU4_GDB.gdb')
+fiona.listlayers(file)
+HUC6 = gpd.read_file(file, layer="WBDHU6")
+
+# %%
+# Added A point
+# Stream gauge in Camp Verde:  34.44833333, -111.7891667
+point_list = np.array([[-111.7891667, 34.44833333]])
+#make these into spatial features
+point_geom = [Point(xy) for xy in point_list]
+point_geom
+
+#map a dataframe of these points
+point_df = gpd.GeoDataFrame(point_geom, columns=['geometry'],
+                            crs=HUC6.crs)
+
+# %%
+# CRS = Coordinate Reference System
+HUC6.crs
+gages.crs
+
+# Projecting the point
+points_project = point_df.to_crs(gages_AZ.crs)
+
+HUC6_project = HUC6.to_crs(gages_AZ.crs)
+
+# %%
+# Final Map
+fig, ax = plt.subplots(figsize=(7, 10))
+gages_AZ.plot(column='DRAIN_SQKM', categorical=False,
+              legend=True, markersize=25, cmap='terrain',
+              ax=ax)
+points_project.plot(ax=ax, color='red', marker='X')
+HUC6_project.boundary.plot(ax=ax, color=None,
+                           edgecolor='black', linewidth=.3)
+ctx.add_basemap(ax, crs=gages_AZ.crs, source = ctx.providers.OpenTopoMap)
+fig.savefig("Group_Proj_Watershed")
